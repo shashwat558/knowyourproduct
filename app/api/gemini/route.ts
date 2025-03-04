a92be28260feb3f607f1c4e8e4e84ff44cdda834
shashwat558 <jainshashwat528@gmail.com>
2025-03-04 23:58:15 +0530

    added gemini for product reviews

---
+++ app/api/gemini/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest){
+    import { GoogleGenerativeAI } from "@google/generative-ai";
+const genAI = new GoogleGenerativeAI("GEMINI_API_KEY");
+
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const prompt = "Explain how AI works";
+
+const result = await model.generateContent(prompt);
+console.log(result.response.text());
+
+}
